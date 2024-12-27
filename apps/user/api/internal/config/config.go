75dba0003f439938824d39e328ab7de0076d3301
yangjiaxin <717138552@qq.com>
2024-12-27 10:58:49 +0800

    updated config

--- apps/user/api/internal/config/config.go
+++ apps/user/api/internal/config/config.go
@@ -7,11 +7,31 @@ import (
 
 type Config struct {
 	rest.RestConf
-	Target  string `json:",env=Target"`
-	Enpoints []string `json:",env=Enpoints"`
+
 	JwtAuth struct {
 		AccessSecret string
 		AccessExpire int64
 	}
-	UserRpc zrpc.RpcClientConf
+	UserRpc  zrpc.RpcClientConf `json:",env=UserRpc""`
+	Target   string             `json:",env=Target"`
+	Endpoint string             `json:",env=Endpoint"`
+}
+
+// RpcClientConfig 用于包装zrpc.RpcClientConf并绑定环境变量
+type RpcClientConfig struct {
+	Etcd          string        `json:",env=Etcd"`
+	Endpoint      string        `json:",env=Endpoint"`
+	Target        string        `json:",env=Target"`
+	Timeout       int64         `json:",env=Timeout"`
+}
+
+// ToRpcClientConf 将包装后的配置转化为 zrpc.RpcClientConf
+func (c *RpcClientConfig) ToRpcClientConf() zrpc.RpcClientConf {
+	var endpoints []string
+	endpoints = append(endpoints, c.Endpoint)
+	return zrpc.RpcClientConf{
+		Endpoints: endpoints,
+		Target:    c.Target,
+		Timeout:   c.Timeout,
+	}
 }
