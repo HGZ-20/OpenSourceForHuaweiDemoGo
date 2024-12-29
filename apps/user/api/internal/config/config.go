4c39a6f22fb2361a2b1aab44356cee50c1c6ebcb
yangjiaxin <717138552@qq.com>
2024-12-29 23:17:13 +0800

    updated config

--- apps/user/api/internal/config/config.go
+++ apps/user/api/internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/zeromicro/go-zero/rest"
-	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type Config struct {
@@ -12,7 +11,7 @@ type Config struct {
 		AccessSecret string
 		AccessExpire int64
 	}
-	UserRpc zrpc.RpcClientConf `json:",env=UserRpc""`
+	UserRpc RpcClientConfig
 	//Target   string             `json:",env=Target"`
 	//Endpoint string             `json:",env=Endpoint"`
 }
@@ -26,12 +25,12 @@ type RpcClientConfig struct {
 }
 
 // ToRpcClientConf 将包装后的配置转化为 zrpc.RpcClientConf
-func (c *RpcClientConfig) ToRpcClientConf() zrpc.RpcClientConf {
-	var endpoints []string
-	endpoints = append(endpoints, c.Endpoint)
-	return zrpc.RpcClientConf{
-		Endpoints: endpoints,
-		Target:    c.Target,
-		Timeout:   c.Timeout,
-	}
-}
+//func (c *RpcClientConfig) ToRpcClientConf() zrpc.RpcClientConf {
+//	var endpoints []string
+//	endpoints = append(endpoints, c.Endpoint)
+//	return zrpc.RpcClientConf{
+//		Endpoints: endpoints,
+//		Target:    c.Target,
+//		Timeout:   c.Timeout,
+//	}
+//}
